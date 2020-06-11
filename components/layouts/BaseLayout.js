b06e1ad61476506d73e45c917d9f1b6e6fa96906
stackblock <stackchain@gmail.com>
2020-06-11 00:04:36 -0400

    middle of working / restart needed

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,11 +1,14 @@
 import Header from "../shared/Header";
 
 const BaseLayout = (props) => {
+  const { className, children } = props;
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {props.children}
-    </>
+      <main className={`cover ${className}`}>
+        <div className="wrapper">{children}</div>
+      </main>
+    </div>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/main.scss";
 
-const App = (props) => {
-  const { Component, pageProps } = props;
-  return <Component {...pageProps} />;
-};
+class App extends React.Component {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    console.log(ctx);
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
 
 export default App;
